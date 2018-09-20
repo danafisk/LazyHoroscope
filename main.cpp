7a8c728481adff538ad69f22f480248644e269e5
danafisk <dfisk@csuchico.edu>
2018-09-19 17:19:44 -0700

    tells fortune

--- main.cpp
+++ main.cpp
@@ -16,6 +16,10 @@ cout<<"What is your last name?";
 cin>>last;
   //tell fortune
 
-cout<<"Welcom "<<first [0] <<"."<< last [0] <<".  here is your fortune...\n";
-  return 0;
+cout<<"Welcome "<<first [0] <<"."<< last [0] <<".  here is your fortune...\n";
+
+cout<<"Your lucky number is " <<first.length() <<".\n";
+
+
+return 0;
 }
