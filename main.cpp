f357c6c48c3b7cbef41c24da7c56fad6762369b5
danafisk <dfisk@csuchico.edu>
2018-09-24 17:17:17 -0700

    tells fortune

--- main.cpp
+++ main.cpp
@@ -16,17 +16,19 @@ cout<<"What is your last name?\n";
 cin>>last;
   //tell fortune
 
-cout<<"Welcome "<<first [0] <<"."<< last [0] <<"., here is your fortune...\n";
+cout<<"Welcome, "<<first [0] <<"."<< last [0] <<"., here is your fortune...\n";
 
-cout<<"Your lucky number is " <<first.length() <<".\n";
+cout<<"your lucky number is " <<first.length() <<"\n";
 
-if (first [0] == 'a'|| first [0] == 'A' || first [0] == 'e' || first [0] == 'E' || first [0] == 'i' || first [0] == 'I' || first [0] == 'o' || first [0] == 'O' || first [0] == 'u' || first [0] == 'U') cout<<"You are destined to be famous!\n";
+if (first [0] == 'a'|| first [0] == 'A' || first [0] == 'e' || first [0] == 'E' || first [0] == 'i' || first [0] == 'I' || first [0] == 'o' || first [0] == 'O' || first [0] == 'u' || first [0] == 'U') cout<<"you are destined to be famous!\n";
 
-else cout<<"Keep a low profile.\n";
+else cout<<"you should keep a low profile.\n";
 
-if (last [0] == 'a'|| last [0] == 'A' || last [0] == 'e' || last  [0] == 'E' || last [0] == 'i' || last [0] == 'I' || last [0]  == 'o' || last [0] == 'O' || last [0] == 'u' || last [0] == 'U') cout<<"You have already met your true love.\n";
+int index = last.length()-1;
 
-cout<<"Have a good day!\n";
+if (last [index] == 'a'|| last [index] == 'A' || last [index] == 'e' || last  [index] == 'E' || last [index] == 'i' || last [index] == 'I' || last [index]  == 'o' || last [index] == 'O' || last [index] == 'u' || last [index] == 'U') cout<<"you have already met your true love.\n";
+
+cout<<"have a good day!\n";
 
 
 return 0;
